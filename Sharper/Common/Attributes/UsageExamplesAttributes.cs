ba22594f677f547504a17df5935bb8f84d62886b
Nexowned <49051663+Nexowned@users.noreply.github.com>
2019-11-18 02:51:17 -0800

    Added some base classes Added GuildConfigurations Added RandomGenerator that utilizes rng

---
+++ Sharper/Common/Attributes/UsageExamplesAttributes.cs
@@ -0,0 +1,45 @@
+﻿#region USING_DIRECTIVES
+using DSharpPlus.CommandsNext;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Linq;
+#endregion;
+
+namespace Sharper.Common.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+    public sealed class UsageExamplesAttributes : Attribute
+    {
+        public string[] Examples { get; private set; }
+
+        public UsageExamplesAttributes(params string[] examples)
+        {
+            if (examples is null)
+                throw new ArgumentException($"No examples provided to {this.GetType().Name}!");
+
+            this.Examples = examples
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Select(s => s.Trim())
+                .ToArray();
+
+            if (!this.Examples.Any())
+                throw new ArgumentException($"Please provide non-empty examples to {this.GetType().Name}!");
+        }
+
+        public string JoinExamples(Command cmd, CommandContext ctx = null, string seperator = "\n")
+        {
+            if (ctx is null)
+                return string.Join(seperator, this.Examples);
+
+            string cname = cmd.QualifiedName;
+            string prefix = ctx.Services.GetService<SharedData>().GetGuildPrefix(ctx.Guild.Id);
+
+            if (cmd.Overloads.Any(o => o.Arguments.All(a => a.IsOptional)))
+                return string.Join(seperator, new[] { "" }.Concat(this.Examples).Select(e => $"{prefix}{cname} {e}"));
+            else
+                return string.Join(seperator, this.Examples.Select(e => $"{prefix}{cname} {e}"));
+        }
+    }
+}
---
+++ Sharper/SharedData.cs
@@ -0,0 +1,42 @@
+﻿#region USING_DIRECTIVES
+using DSharpPlus;
+using DSharpPlus.Entities;
+
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+#endregion
+
+namespace Sharper
+{
+    public sealed class SharedData : IDisposable
+    {
+        // fields
+
+        public SharedData()
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        #region CHANNEL_EVENT_HELPERS
+        // todo
+        #endregion
+
+        #region RANK_HELPERS
+        #endregion
+
+        #region GUILD_DATA_HELPERS
+        #endregion
+
+        #region PENDING_RESPONSES_HELPERS
+        #endregion
+    }
+}
