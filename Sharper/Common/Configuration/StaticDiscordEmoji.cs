44ca851baf1ac8d3d9489e0cdd449583059867d7
Nexowned <49051663+Nexowned@users.noreply.github.com>
2019-11-18 03:29:22 -0800

    Added various tasks Added custom ip string format for api's Added channel event helper class Added base class to store all emojis used in commands and instances

---
+++ Sharper/Common/Configuration/StaticDiscordEmoji.cs
@@ -0,0 +1,14 @@
+﻿#region USING_DIRECTIVES
+using DSharpPlus.Entities;
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+#endregion
+
+namespace Sharper.Common.Configuration
+{
+    public static class StaticDiscordEmoji
+    {
+        // class to store all emojis for related events
+    }
+}
---
+++ Sharper/Common/CustomIPFormat.cs
@@ -0,0 +1,35 @@
+﻿#region USING_DIRECTIVES
+using System;
+using System.Text.RegularExpressions;
+#endregion;
+
+namespace Sharper.Common
+{
+    public sealed class CustomIPFormat
+    {
+        public string Content { get; set; }
+
+        public CustomIPFormat(string format)
+        {
+            if (string.IsNullOrWhiteSpace(format))
+                throw new ArgumentException("IP format cannot be null.");
+            this.Content = format;
+        }
+
+        private static readonly Regex _parseRegex = new Regex(@"^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|\*)((\.|$)(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|\*)){0,3}(:[0-9]{4,5})?$", RegexOptions.Compiled);
+
+        public static bool TryParse(string str, out CustomIPFormat res)
+        {
+            if (_parseRegex.IsMatch(str))
+            {
+                res = new CustomIPFormat(str);
+                return true;
+            }
+            else
+            {
+                res = null;
+                return false;
+            }
+        }
+    }
+}
