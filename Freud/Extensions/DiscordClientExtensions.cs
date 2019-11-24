1654d7da61fa61f40cf28f21034f36c0e7fd3c49
Nexowned <49051663+Nexowned@users.noreply.github.com>
2019-11-24 10:25:25 -0800

    Added a Discord "Client" extension This will create an asynchronous Dm channel with the guild (server) that it is in

---
+++ Freud/Extensions/DiscordClientExtensions.cs
@@ -0,0 +1,24 @@
+﻿#region USING_DIRECTIVES
+
+using DSharpPlus;
+using DSharpPlus.Entities;
+using System.Threading.Tasks;
+
+#endregion USING_DIRECTIVES
+
+namespace Freud.Extensions
+{
+    internal static class DiscordClientExtensions
+    {
+        public static Task<DiscordDmChannel> CreateDmChannelAsync(this DiscordClient client, ulong uid)
+        {
+            foreach ((ulong gid, var guild) in client.Guilds)
+            {
+                if (guild.Members.TryGetValue(uid, out var member))
+                    return member?.CreateDmChannelAsync() ?? Task.FromResult<DiscordDmChannel>(null);
+            }
+
+            return Task.FromResult<DiscordDmChannel>(null);
+        }
+    }
+}
