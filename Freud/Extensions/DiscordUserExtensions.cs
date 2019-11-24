d413795fbfad418c406980c3ef8f26a7dc8b7f85
Nexowned <49051663+Nexowned@users.noreply.github.com>
2019-11-24 10:28:14 -0800

    Added an extension for "Discord Users" This extension will allow us to tell if a userif part of the requested guild is in that guild, or not in that guild ( or server )

---
+++ Freud/Extensions/DiscordUserExtensions.cs
@@ -0,0 +1,26 @@
+﻿#region USING_DIRECTIVES
+
+using DSharpPlus.Entities;
+using System.Threading.Tasks;
+
+#endregion USING_DIRECTIVES
+
+namespace Freud.Extensions
+{
+    internal static class DiscordUserExtensions
+    {
+        public static async Task<bool> IsMemberOfGuildAsync(this DiscordUser u, DiscordGuild g)
+        {
+            try
+            {
+                var m = await g.GetMemberAsync(u.Id);
+                return true;
+            } catch
+            {
+                // Not found ...
+            }
+
+            return false;
+        }
+    }
+}
