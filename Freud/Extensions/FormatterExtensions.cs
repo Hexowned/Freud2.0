1302ecb9101f42b2465446488e7561cdbbc957eb
Nexowned <49051663+Nexowned@users.noreply.github.com>
2019-11-24 10:17:28 -0800

    Added a Formatter extension This allows easy access to discords "Spoiler" feature and strips markdown by using a regex algo for reading regular expressions

---
+++ Freud/Extensions/FormatterExtensions.cs
@@ -0,0 +1,19 @@
+﻿#region USING_DIRECTIVES
+
+using System.Text.RegularExpressions;
+
+#endregion USING_DIRECTIVES
+
+namespace Freud.Extensions
+{
+    public static class FormatterExtensions
+    {
+        private static readonly Regex MarkdownStripRegex = new Regex(@"([`\*_~\[\]\(\)""])", RegexOptions.ECMAScript);
+
+        public static string Spoiler(string str)
+            => $"||{str}||";
+
+        public static string StripMarkdown(string str)
+            => MarkdownStripRegex.Replace(str, m => string.Empty);
+    }
+}
