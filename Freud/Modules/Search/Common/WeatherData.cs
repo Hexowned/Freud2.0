7f91c79d5828df32437ee2167b16f6ddbf8f780f
Nexowned <49051663+Nexowned@users.noreply.github.com>
2019-11-25 13:44:26 -0800

    Added a weather module that grabs data from the openweatherapp api

---
+++ Freud/Modules/Search/Common/WeatherData.cs
@@ -0,0 +1,96 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Freud.Modules.Search.Common
+{
+    public class WeatherData
+    {
+        public List<Weather> Weather { get; set; }
+        public Main Main { get; set; }
+        public int Visibility { get; set; }
+        public Wind Wind { get; set; }
+        public Clouds Clouds { get; set; }
+        public int Dt { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Cod { get; set; }
+        public Sys Sys { get; set; }
+    }
+
+    public class PartialWeatherData
+    {
+        public List<Weather> Weather { get; set; }
+        public Main Main { get; set; }
+        public int Visibility { get; set; }
+        public Wind Wind { get; set; }
+        public Clouds Clouds { get; set; }
+        public int Dt { get; set; }
+        public int Id { get; set; }
+        public int Cod { get; set; }
+        public Sys Sys { get; set; }
+    }
+
+    public class Forecast
+    {
+        public City City { get; set; }
+
+        [JsonProperty("list")]
+        public List<PartialWeatherData> WeatherDataList { get; set; }
+    }
+
+    public class Coord
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+
+    public class City
+    {
+        public Coord Coord { get; set; }
+        public string Country { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Weather
+    {
+        public string Description { get; set; }
+        public string Icon { get; set; }
+        public int Id { get; set; }
+        public string Main { get; set; }
+    }
+
+    public class Main
+    {
+        public double Temperature { get; set; }
+        public float Pressure { get; set; }
+        public float Humidity { get; set; }
+
+        [JsonProperty("temp_min")]
+        public double TemperatureMin { get; set; }
+
+        [JsonProperty("temp_max")]
+        public double TemperatureMax { get; set; }
+    }
+
+    public class Wind
+    {
+        public double Speed { get; set; }
+        public double Degrees { get; set; }
+    }
+
+    public class Clouds
+    {
+        public int All { get; set; }
+    }
+
+    public class Sys
+    {
+        public int Type { get; set; }
+        public int Id { get; set; }
+        public double Message { get; set; }
+        public string Country { get; set; }
+        public double Sunrise { get; set; }
+        public double Sunset { get; set; }
+    }
+}
---
+++ Freud/Modules/Search/WeatherModule.cs
@@ -0,0 +1,78 @@
+﻿#region USING_DIRECTIVES
+
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
+using Freud.Common.Attributes;
+using Freud.Database.Db;
+using Freud.Exceptions;
+using Freud.Modules.Search.Services;
+using System.Linq;
+using System.Threading.Tasks;
+
+#endregion USING_DIRECTIVES
+
+namespace Freud.Modules.Search
+{
+    [Group("weather"), Module(ModuleType.Searches), NotBlocked]
+    [Description("Weather search commands. Group call returns weather information for given query.")]
+    [Aliases("w")]
+    [UsageExampleArgs("london")]
+    [Cooldown(3, 5, CooldownBucketType.Channel)]
+    public class WeatherModule : FreudServiceModule<WeatherService>
+    {
+        public WeatherModule(WeatherService weather, SharedData shared, DatabaseContextBuilder db)
+            : base(weather, shared, db)
+        {
+            this.ModuleColor = DiscordColor.Aquamarine;
+        }
+
+        [GroupCommand]
+        public async Task ExecuteGroupAsync(CommandContext ctx,
+                                           [RemainingText, Description("Query.")] string query)
+        {
+            if (this.Service.IsDisabled())
+                throw new ServiceDisabledException();
+
+            if (string.IsNullOrWhiteSpace(query))
+                throw new InvalidCommandUsageException("You need to specify a query (city usually).");
+
+            var em = await this.Service.GetEmbeddedCurrentWeatherDataAsync(query);
+            if (em is null)
+                throw new CommandFailedException("Cannot find weather data for given query.");
+
+            await ctx.RespondAsync(embed: em);
+        }
+
+        #region COMMAND_WEATHER_FORECAST
+
+        [Command("forecast"), Priority(1)]
+        [Description("Get weather forecast for the following days (def: 7).")]
+        [Aliases("f")]
+        [UsageExampleArgs("london", "5 london")]
+        public async Task ForecastAsync(CommandContext ctx,
+                                       [Description("Amount of days to fetch the forecast for.")] int amount,
+                                       [RemainingText, Description("Query.")] string query)
+        {
+            if (this.Service.IsDisabled())
+                throw new ServiceDisabledException();
+
+            if (string.IsNullOrWhiteSpace(query))
+                throw new InvalidCommandUsageException("You need to specify a query (city usually).");
+
+            var ems = await this.Service.GetEmbeddedWeatherForecastAsync(query, amount);
+            if (ems is null || !ems.Any())
+                throw new CommandFailedException("Cannot find weather data for given query.");
+
+            await ctx.Client.GetInteractivity().SendPaginatedMessageAsync(ctx.Channel, ctx.User, ems.Select(e => new Page(embed: e)));
+        }
+
+        [Command("forecast"), Priority(0)]
+        public Task ForecastAsync(CommandContext ctx,
+                                 [RemainingText, Description("Query.")] string query)
+            => this.ForecastAsync(ctx, 7, query);
+
+        #endregion COMMAND_WEATHER_FORECAST
+    }
+}
