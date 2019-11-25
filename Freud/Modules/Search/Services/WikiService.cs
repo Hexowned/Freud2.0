cbfb291a97020e16b5eb37f157da83d6b0037371
Nexowned <49051663+Nexowned@users.noreply.github.com>
2019-11-25 14:01:07 -0800

    Added a wiki module that uses the wiki api to do query searches

---
+++ Freud/Modules/Search/Services/WikiService.cs
@@ -0,0 +1,52 @@
+﻿#region USING_DIRECTIVES
+
+using Freud.Modules.Search.Common;
+using Freud.Services;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+#endregion USING_DIRECTIVES
+
+namespace Freud.Modules.Search.Services
+{
+    public class WikiService : FreudHttpService
+    {
+        public static readonly string WikipediaIconUrl = "https://en.wikipedia.org/static/images/project-logos/enwiki.png";
+
+        private static readonly string _url = "https://en.wikipedia.org/w/api.php?action=opensearch&limit=20&namespace=0&format=json&search=";
+        private static readonly SemaphoreSlim _requestSemaphore = new SemaphoreSlim(1, 1);
+
+        public override bool IsDisabled()
+            => false;
+
+        public static async Task<WikiSearchResponse> SearchAsync(string query)
+        {
+            if (string.IsNullOrWhiteSpace(query))
+                throw new ArgumentException("Query missing", nameof(query));
+
+            string result = await _http.GetStringAsync($"{_url}{WebUtility.UrlEncode(query)}").ConfigureAwait(false);
+
+            await _requestSemaphore.WaitAsync();
+            try
+            {
+                var jarr = JArray.Parse(result);
+
+                var tquery = jarr.First;
+                var thits = tquery.Next;
+                var tsnippets = thits.Next;
+                var turls = tsnippets.Next;
+
+                return new WikiSearchResponse(tquery.ToString(), thits.ToObject<string[]>(), tsnippets.ToObject<string[]>(), turls.ToObject<string[]>());
+            } catch
+            {
+                return null;
+            } finally
+            {
+                _requestSemaphore.Release();
+            }
+        }
+    }
+}
---
+++ Freud/Modules/Search/WikiModule.cs
@@ -0,0 +1,63 @@
+﻿#region USING_DIRECTIVES
+
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
+using Freud.Common.Attributes;
+using Freud.Database.Db;
+using Freud.Modules.Search.Services;
+using System.Linq;
+using System.Threading.Tasks;
+
+#endregion USING_DIRECTIVES
+
+namespace Freud.Modules.Search
+{
+    [Group("wikipedia"), Module(ModuleType.Searches), NotBlocked]
+    [Description("Wikipedia search. If invoked without a subcommand, searches Wikipedia with given query.")]
+    [Aliases("wiki")]
+    [UsageExampleArgs("Linux")]
+    [Cooldown(3, 5, CooldownBucketType.Channel)]
+    public class WikiModule : FreudModule
+    {
+        public WikiModule(SharedData shared, DatabaseContextBuilder db)
+           : base(shared, db)
+        {
+            this.ModuleColor = DiscordColor.White;
+        }
+
+        [GroupCommand]
+        public Task ExecuteGroupAsync(CommandContext ctx,
+                                     [RemainingText, Description("Query.")] string query)
+            => this.SearchAsync(ctx, query);
+
+        #region COMMAND_WIKI_SEARCH
+
+        [Command("search")]
+        [Description("Search Wikipedia for a given query.")]
+        [Aliases("s", "find")]
+        [UsageExampleArgs("Linux")]
+        public async Task SearchAsync(CommandContext ctx,
+                                    [RemainingText, Description("Query.")] string query)
+        {
+            var res = await WikiService.SearchAsync(query);
+            if (res is null || !res.Any())
+            {
+                await this.InformOfFailureAsync(ctx, "No results...");
+                return;
+            }
+
+            await ctx.Client.GetInteractivity().SendPaginatedMessageAsync(ctx.Channel, ctx.User, res.Select(r
+                => new Page(embed: new DiscordEmbedBuilder
+                {
+                    Title = r.Title,
+                    Description = string.IsNullOrWhiteSpace(r.Snippet) ? "No description provided" : r.Snippet,
+                    Url = r.Url,
+                    Color = this.ModuleColor
+                }.WithFooter("Powered by Wikipedia API", WikiService.WikipediaIconUrl))));
+        }
+
+        #endregion COMMAND_WIKI_SEARCH
+    }
+}
